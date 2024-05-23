9ef1c9e0e60a216d08f6f353b26fdc8bda83f065
Ryhox <emanuelpfeifer1@gmail.com>
2024-05-23 20:46:50 +0200

    Fixed Structure

---
+++ scripts/mainscript.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("wheel-and-hamster").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }, 1000);
+});
+function link1() {
+    
+    window.location.href = "https://www.pinterest.it/search/pins/?q=cats&rs=typed";
+}
+function link2() {
+    window.location.href = "aboutme.html";
+}
+function link3() {
+    window.location.href = "games.html";
+}
