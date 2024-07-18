1ff2a81c8d4dac73836e40790f871d0f8c0db2f1
Ryhox <emanuelpfeifer1@gmail.com>
2024-07-18 13:21:55 +0200

    Updates

---
+++ scripts/mainscript.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }, 1000);
+});
+
+
+
+  
+  
+  
